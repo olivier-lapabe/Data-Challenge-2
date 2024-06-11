81a44bf960c0393daac0a606fdd38fb762a27727
Lucas <wakuucomptes@gmail.com>
2024-06-11 16:07:20 +0200

    Resnet101

--- bin/Train_runner_resnet101.py
+++ bin/Train_runner_resnet101.py
@@ -2,25 +2,23 @@ import os
 import torch
 import torch.nn as nn
 import torchvision
-from torchvision import models
-from src.DataLoader.DataLoader import create_trainval_dataloaders
-from src.Solver.Solver import Solver
+from torchvision.models import resnet101, ResNet101_Weights
+from src.Solver.ROP_Solver import Solver
 from src.utils import define_device
-from src.CustomLoss import CustomLoss
 from src.DataLoader.HDF5_DataLoader_tensor import create_tensor_dataloaders
 
 
 def main():
     # Test name (used for the name of the results folder)
-    test_name = "resnet101_pretrained"
+    test_name = "resnet101_256_250_ROP_REG"
 
     # Dataloader parameters
     n_val = 20000
-    batch_size = 32
+    batch_size = 256
     num_workers = int(os.cpu_count() / 2)
 
     # Initialize ResNet-101 with pretrained weights
-    model = models.resnet101(pretrained=True)
+    model = resnet101(weights=ResNet101_Weights.DEFAULT)
     num_ftrs = model.fc.in_features
     # Change to output a single continuous value for regression
     model.fc = nn.Linear(num_ftrs, 1)
@@ -28,14 +26,16 @@ def main():
     # Loss function and optimizer
     loss_fn = nn.MSELoss()
     # Consider using a smaller learning rate
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-    num_epochs = 500
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=0.0001, weight_decay=1e-5)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    num_epochs = 250
 
     # Create Training, Validation dataloaders
     trainval_dataloaders = create_tensor_dataloaders(
         batch_size=batch_size,
         num_workers=num_workers,
-        path="./images_dataset_normalized.hdf5")
+        path="./images_dataset_resnet.hdf5")
 
     # Define device
     device = define_device()
